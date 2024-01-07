83d29ac69d224896f8dd23b12c2ca57eeb731633
Arina Aleevskaya <seawolfy60@gmail.com>
2024-01-07 11:51:40 +0300

    Tests (#3)
    * django initial
    
    * user model
    
    * price model
    
    * price works
    
    * env sample
    
    * update readme
    
    * alter full price method
    
    * flake 8
    
    * user api
    
    * update readme
    
    * tests for users and price

--- price/tests.py
+++ price/tests.py
@@ -1,3 +1,63 @@
-# from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from price.models import Price
+from users.models import User
+
+
+class PriceTestCase(APITestCase):
+    """Price model testing"""
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.vendor = User.objects.create_user(
+            first_name='TestName',
+            last_name='TestLastName',
+            username='vendor',
+            password='1234',
+            is_vendor=True,
+            email='tester@test.com'
+        )
+
+    def test_create_price_unauthenticated(self):
+        data = {
+            'origin_price': "100",
+        }
+
+        response = self.client.post(
+            path='/prices/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_401_UNAUTHORIZED
+        )
+
+    def test_create_price_authenticated(self):
+        self.client.force_authenticate(user=self.vendor)
+
+        data = {
+            'origin_price': "100",
+        }
+
+        response = self.client.post(
+            path='/prices/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            response.json(),
+            {"origin_price": "100.000000",
+             "full_price": 130.56,
+             "tax": 0.06,
+             "bank_commission": 0.02,
+             "shop_commission": 0.2}
+        )
+
+        self.assertTrue(Price.objects.all().exists())
