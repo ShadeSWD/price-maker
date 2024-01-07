0e3dd0d61f9a8ce06759d00007d870360e4b1b62
ShadeSWD <seawolfy60@gmail.com>
2024-01-07 11:49:03 +0300

    tests for users and price

--- price/permissions.py
+++ price/permissions.py
@@ -3,6 +3,10 @@ from rest_framework.permissions import BasePermission
 
 class IsVendor(BasePermission):
     def has_permission(self, request, view):
+        try:
+            request.user.is_vendor
+        except AttributeError:
+            return False
         if view.action in ('destroy', ):
             return False
         if view.action in ('create', 'list', 'retrieve', 'update', 'partial_update'):
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,55 @@
-# from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserTestCase(APITestCase):
+    """Price model testing"""
+
+    def setUp(self) -> None:
+        self.admin = User.objects.create_superuser(username='admin', password='admin')
+
+    def test_create_user_unauthenticated(self):
+        data = {
+            "first_name": 'TestName',
+            "last_name": 'TestLastName',
+            "username": 'vendor',
+            "password": '1234',
+            "is_vendor": "True",
+            "email": 'tester@test.com'
+        }
+
+        response = self.client.post(
+            path='/users/prices/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_401_UNAUTHORIZED
+        )
+
+    def test_create_user_authenticated(self):
+        self.client.force_authenticate(user=self.admin)
+
+        data = {
+            "first_name": 'TestName',
+            "last_name": 'TestLastName',
+            "username": 'vendor',
+            "password": '1234',
+            "is_vendor": "True",
+            "email": 'tester@test.com'
+        }
+
+        response = self.client.post(
+            path='/users/prices/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertTrue(User.objects.all().exists())
