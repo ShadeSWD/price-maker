357708180efa72753aa35cda898aeb072ed1b6fd
ShadeSWD <seawolfy60@gmail.com>
2024-01-07 06:18:57 +0300

    user api

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from users.apps import UsersConfig
+from users.views import UsersViewSet
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from users.apps import UsersConfig
 app_name = UsersConfig.name
 
+router = DefaultRouter()
+router.register(r'prices', UsersViewSet, basename='prices')
+
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+              ] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-# from django.shortcuts import render
+from rest_framework import viewsets
+from users.models import User
+from users.serializers import UserSerializer
+from users.paginators import UserPagination
+from users.permissions import IsStaff
 
-# Create your views here.
+
+class UsersViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    pagination_class = UserPagination
+    permission_classes = [IsStaff, ]
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        new = serializer.save()
+        new.set_password(new.password)
+        new.save()
